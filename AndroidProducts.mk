cea005cd3f493e3cca4c5e1229b841041085a111
winbo-yml-exe <winbo.yml@proton.me>
2024-12-29 10:27:26 +0100

    xcover4lte: adjust device tree to OrangeFox standards

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_xcover4lte.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_xcover4lte.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo omni_xcover4lte-eng
+add_lunch_combo twrp_xcover4lte-eng
